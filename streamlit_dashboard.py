94a30f20ffb560f4bad5b003b06bf924de8b452b
aheyler <66250789+aheyler@users.noreply.github.com>
2022-01-06 20:11:32 -0700

    updates

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -9,7 +9,7 @@ import statsmodels
 
 st.title("Analyzing Movies")
 st.image("https://s3-us-west-2.amazonaws.com/prd-rteditorial/wp-content/uploads/2018/03/13153742/RT_300EssentialMovies_700X250.jpg","Source:Rotten Tomatoes")
-st.caption("In this dashboard, we investigate movies––their ratings, revenues, genres, streaming platforms, and directors. We were curious to see how these attributes changed over time as well as how they related to one another. The following graphs illustrate our key findings. ")
+st.caption("In this dashboard, we investigate movies––their ratings, revenues, genres, streaming platforms, and directors. We were curious to see how these attributes changed over time as well as how they relate to one another. The following graphs illustrate our key findings. ")
 mcol1,mcol2,mcol3,mcol4=st.columns(4)
 mcol1.metric("Movies Collected","1000")
 mcol2.metric("Directors Analyzed","401")
@@ -31,7 +31,7 @@ def director_stats_df(movie_df):
     for director in directors:
         director_stats.loc[director,'rating']=movie_df[movie_df.Director==director]['IMDB_Rating'].mean()
         director_stats.loc[director,'Number of Movies Made']=len(movie_df[movie_df.Director==director])
-        director_stats.loc[director,'Avg Domestic Revenue']=movie_df[movie_df.Director==director]['Gross_Rev'].mean()
+        director_stats.loc[director,'Average Domestic Revenue']=movie_df[movie_df.Director==director]['Gross_Rev'].mean()
         director_stats.sort_index(inplace = True) 
     return director_stats
 director_stats=director_stats_df(movie_df)  
@@ -40,7 +40,7 @@ director_stats.rename({'index': 'director'}, axis=1, inplace=True)
 director_stats['rating']=director_stats["rating"].apply(lambda x: int(x))
 figd = px.scatter(director_stats, x='Number of Movies Made', y='Avg Domestic Revenue',
                  hover_data=['director'],size='rating',
-                 title= 'Directors Avg. Revenue Information')
+                 title= 'Which directors were most successful?')
 figd.update_layout(autosize=False,width=800,height=600,)
 figd.update_layout(legend=dict(
     orientation="h",
@@ -82,7 +82,7 @@ elif option=="Ratings":
     movie_years.drop(movie_years.index[movie_years['Released_Year'] == 'PG'], inplace = True)
     movie_years['Year']=movie_df['Released_Year'].apply(lambda x: int(x))
     movie_years.sort_values('Year',inplace=True)
-    figrm = px.scatter(movie_years, x="Year", y="IMDB_Rating", hover_data = ["Series_Title"])
+    figrm = px.scatter(movie_years, x="Year", y="IMDB_Rating", hover_data = ["Series_Title"], title="Ratings Over Time")
     st.plotly_chart(figrm)
     st.caption("There is no definitive trend between movie ratings over the years. As the world progresses, more movies are made each year, allowing there to be a variety of ratings. Since more movies are being made, there is a greater number of movies past 1970 with a rating of 8.8 or higher. However, from 2010-2020 there were no movies with a rating of an 8.8 or higher")
 
@@ -97,20 +97,14 @@ if rating_options=="Runtime":
     st.caption("The data shows that movies longer than 2 hours have ratings above 8.6, except for '12 Angry Men'. The bulk of the data shows no distinct correlation between movie length and rating, but it can be interpretted that longer movies outliers are rated higher. ")
 elif rating_options=="Votes":
     # Movie ratings vs Votes
-    figRV = px.scatter(movie_df, x="IMDB_Rating", y="No_of_Votes",color="Series_Title")
+    figRV = px.scatter(movie_df, x="IMDB_Rating", y="No_of_Votes",hover_data=["Series_Title"])
     st.plotly_chart(figRV)
     st.caption("Most movies that are rated above an 8.5 earn less than 400 million dollars. The correlation between ratings and revenue shows that a movie does not have to be highly rated to earn a high revenue. ")
 elif rating_options=="Revenue":
     #movie ratings vs revenue
-    figRR = px.scatter(movie_df, x="IMDB_Rating", y="Gross", color = "Series_Title", size ='IMDB_Rating', title= "Rating and Revenue Comparison")
+    figRR = px.scatter(movie_df, x="IMDB_Rating", y="Gross", hover_data = ["Series_Title"], size ='IMDB_Rating', title= "Rating and Revenue Comparison")
     st.plotly_chart(figRR)
     st.caption("The number of votes that a movie received increased as the rating went up. ")
-
-
-
-
-
-
 
 
 # Nathan & Emilio
@@ -154,38 +148,35 @@ def create_maingenre_df():
 maingenre_df = create_maingenre_df()
 
 #lists for box/whiskers
-counts_per_genre = {"Drama": sum(maingenre_df["Drama"]), "Comedy": sum(maingenre_df["Comedy"]),"Romance": sum(maingenre_df["Romance"]), "Action": sum(maingenre_df["Action"]), "Sci-Fi": sum(maingenre_df["Sci-Fi"]), "Thriller": sum(maingenre_df["Thriller"])}
-genre_df = maingenre_df.from_dict(counts_per_genre, orient = 'index', columns=['number_of_movies'])
-avg_per_genre = [77.54,77.53, 80.22, 73.21, 78.24, 75.83 ]
-min_per_genre = [28, 41, 45, 30, 30, 30 ]
-max_per_genre = [100, 99, 100, 98, 98, 97]
-twentyfive_per_genre = [70, 70, 72, 64, 73, 69.5]
-fifty_per_genre = [79, 80.5, 83, 75, 80, 77]
-seventyfive_per_genre = [87, 87, 89, 83, 89, 85]
-realmin_per_genre = [44.5, 44.5, 46.5, 43.5, 55.5, 46.25]
-genre_df["Meta_score_avg"] = avg_per_genre
-genre_df["Meta_score_min"] = min_per_genre
-genre_df["Meta_score_max"] = max_per_genre
-genre_df["Meta_score_twentyfive"] = twentyfive_per_genre
-genre_df["Meta_score_fifty"] = fifty_per_genre
-genre_df["Meta_score_seventyfive"] = seventyfive_per_genre 
-genre_df["Meta_score_realmin"] = realmin_per_genre
+def create_genre_df():
+    counts_per_genre = {"Drama": sum(maingenre_df["Drama"]), "Comedy": sum(maingenre_df["Comedy"]),"Romance": sum(maingenre_df["Romance"]), "Action": sum(maingenre_df["Action"]), "Sci-Fi": sum(maingenre_df["Sci-Fi"]), "Thriller": sum(maingenre_df["Thriller"])}
+    genre_df = maingenre_df.from_dict(counts_per_genre, orient = 'index', columns=['number_of_movies'])
+    avg_per_genre = [77.54,77.53, 80.22, 73.21, 78.24, 75.83 ]
+    min_per_genre = [28, 41, 45, 30, 30, 30 ]
+    max_per_genre = [100, 99, 100, 98, 98, 97]
+    twentyfive_per_genre = [70, 70, 72, 64, 73, 69.5]
+    fifty_per_genre = [79, 80.5, 83, 75, 80, 77]
+    seventyfive_per_genre = [87, 87, 89, 83, 89, 85]
+    realmin_per_genre = [44.5, 44.5, 46.5, 43.5, 55.5, 46.25]
+    genre_df["Meta_score_avg"] = avg_per_genre
+    genre_df["Meta_score_min"] = min_per_genre
+    genre_df["Meta_score_max"] = max_per_genre
+    genre_df["Meta_score_twentyfive"] = twentyfive_per_genre
+    genre_df["Meta_score_fifty"] = fifty_per_genre
+    genre_df["Meta_score_seventyfive"] = seventyfive_per_genre 
+    genre_df["Meta_score_realmin"] = realmin_per_genre
+    return genre_df
 
-
-
-
-
+genre_df = create_genre_df()
 rows = genre_df.index
 
-st.title('Distribution of Genres')
-
-fig = px.bar(genre_df, x=rows, y="number_of_movies", color = rows, title="Distribution of Genres Across the Top 700 Movies")
-st.plotly_chart(fig)
+# fig = px.bar(genre_df, x=rows, y="number_of_movies", color = rows, title="Distribution of Genres Across the Top 700 Movies")
+# st.plotly_chart(fig)
 
 fig = px.pie(genre_df, values="number_of_movies", names=rows, title="Distribution of Genres Across the Top 700 Movies")
 st.plotly_chart(fig)
 
-st.caption("Data shown using different visualizations. Both display the number of movies across different genres among the top 700 movies. The drama genre seems to be the most popular")
+st.caption("This pie chart shows the number of movies across different genres among the top 700 movies. The drama genre seems to be the most popular among the top 1000 IMDB movies.")
 
 fig = px.scatter(rating_df, x="IMDB_Rating", y="Gross", color="Drama", hover_data = ["Series_Title"])
 st.plotly_chart(fig)
@@ -196,7 +187,6 @@ fig = px.box(genre_df, x=rows, y=['Meta_score_min',"Meta_score_max", "Meta_score
 st.plotly_chart(fig)
 
 st.caption("Box and Whisker plot describing the range of ratings across multiple movie genres. The figure shows that the Romance genre is most consistently highest rated on the Meta Score platform.")
-
 
 
 # Alina & Saul
@@ -239,6 +229,6 @@ elif platform_selected=="Disney+":
         
 # CONCLUSION- don't forget to add to this :)
 st.header("Key Takeaways")
-st.caption("Takeaways from section #1")
+st.caption("Looking at directors of top 1000 movies, Steven Spielberg has directed the most movies, while Anthony Russo has the highest average IMDB ratings for his movies. ")
 st.caption("Takeaways from section #2 - From our data, we were able to see that movies from the drama genre are most popular in the list of the top 700 movies. For the correlation of revenue to IMDB rating, we found that there is no correlation rating and gross income. With this scale of rating, our charts also show that drama movies rank highly most consistently. In the spread of rating, in terms of a movies Meta Score, drama movies had the highest deviation in score, while romance movies were most consistently highly ranked with relatively minimal spread of score. ")
 st.caption("Takeaways from section #3 - In conclusion our data shows that movie streaming services can cater to specific audiences based on their age group like how Disney+ has most of the movies in the all and 7+ category, while Prime Video has most of the movies in the 18+ section. Another note-worthy takeaway is that Rotten Tomatoe ratings do not have any clear correlations to age group.")
